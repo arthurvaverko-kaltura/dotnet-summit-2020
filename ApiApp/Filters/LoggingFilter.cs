7c7960e2e7ea0a4eb82a874c10bd3dac32a4d1ca
Arthur Vaverko <arthur.vaverko@kaltura.com>
2020-08-02 18:10:30 +0300

    moved LoggingContextHandler to correct place

---
+++ ApiApp/Filters/LoggingFilter.cs
@@ -0,0 +1,21 @@
+﻿using KLogMonitor;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace ApiApp
+{
+    internal class LoggingContextHandler : DelegatingHandler
+    {
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            HttpContext.Current.Items[KLogger.REQUEST_ID_KEY] = request.GetCorrelationId();
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
